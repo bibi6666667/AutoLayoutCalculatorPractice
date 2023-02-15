715622d30f3c6a4504b293ae372e03e9d87c0e2d
Bibi <bibi6666667@kakao.com>
2023-02-15 17:27:07 +0900

    Calc StackView (2) Code

--- CalculatorPractice/ViewController.swift
+++ CalculatorPractice/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var button1: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultLabel.layer.cornerRadius = 20
+        resultLabel.clipsToBounds = true // resultLabel.layer.masksToBounds = true
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        button1.layer.cornerRadius = button1.bounds.width / 2
+    }
 
 }
 
